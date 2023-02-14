fa4a3cc839878d5a177b3b74412488dec2e6727f
AN-dragon <spectdragoniv@gmail.com>
2023-02-14 14:16:12 +0000

    IF-estrutura

--- EstruturaDeSelecao.java
+++ EstruturaDeSelecao.java
@@ -1,3 +1,19 @@
 public class EstruturaDeSelecao {
     
+    public static void main(String[] args){
+
+        int idade;
+        idade = 19;
+        /* 'idade' é uma variável */
+
+        if (idade >= 18)
+        {
+
+            System.out.print( "apto ao Serviço Militar Obrigatório!");
+                        /* o 's:' é automatico btw */
+
+        } else {
+            System.out.print( "Não está apto ao Serviço Militar Obrigatório!");
+        }
+    }
 }
