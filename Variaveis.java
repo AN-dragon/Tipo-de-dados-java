9a1c1bd009b234b9fd4ce9227a92e7a282f36418
AN-dragon <spectdragoniv@gmail.com>
2023-02-14 14:59:45 +0000

    Variaveis v3

--- Variaveis.java
+++ Variaveis.java
@@ -2,10 +2,22 @@ public class Variaveis {
     
     public static void main(String[] args) {
 
-        int x;
+        int x, y, soma;
         x = 100;
+        y = 74;
+        soma = x + y;
+
+        /* double -- numero decimal */
+        double z;
+        z = 25.7;
 
         System.out.println("Você tem \n" + x);
+        System.out.println("Você também tem \n" + y);
+        System.out.println("A soma deles são \n" + soma);
+      /* '\n' serve para quebrar uma linha, e da para usar vários na mesma linha */
+      /* 'println' -- pula uma linha para o texto */
+
+      System.out.println("btw você também tem " + z);
     }
 
 }
