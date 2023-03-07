72284e247497b6e0517373d1487d5d2f4f15a7cc
AN-dragon <spectdragoniv@gmail.com>
2023-03-07 14:48:28 +0000

    JavaComboMad

--- atv05_Pizza.java
+++ atv05_Pizza.java
@@ -7,23 +7,42 @@ public class atv05_Pizza {
         //Exercício 4
 
         Scanner input = new Scanner(System.in);
-        String nome;
-        int idade;
+        String nome1, nome2;
+        int idade1, idade2, dif_idade;
+
 
         //Primeira pessoa
-        System.out.println("pessoa1, diga seu nomeUwU: ");
-        nome = input.next();
+        System.out.println("Primeiro usuário, diga seu nome: ");
+        nome1 = input.next();
 
-        System.out.println("agora sua idade: ");
-        idade = input.nextInt();
+        System.out.println("E agora, sua idade: ");
+        idade1 = input.nextInt();
 
         //Segunda pessoa
-        System.out.println("pessoa2, diga seu nomeUwU: ");
-        nome = input.next();
+        System.out.println("Segundo usuário, diga seu nome: ");
+        nome2 = input.next();
 
-        System.out.println("agora sua idade: ");
-        idade = input.nextInt();
+        System.out.println("E agora, sua idade: ");
+        idade2 = input.nextInt();
 
 
+        if (idade1 > idade2) {
+            System.out.printf(nome1 + " com %s anos, é mais velho que %s.", idade1, nome2);
+            
+            //Diferença de idade
+            dif_idade = idade1 - idade2;
+            System.out.printf("\nDiferença de %s anos!", dif_idade);
+
+        } else if (idade2 > idade1) {
+            System.out.printf(nome2 + " com %s anos, é mais velho que %s.", idade2, nome1);
+
+
+            dif_idade = idade2 - idade1;
+            System.out.printf("\nDiferença de %s anos!", dif_idade);
+            
+        } else {
+            System.out.printf("%s e %s tem a mesma idade uwu", nome1, nome2);
+
+        }
     }
 }
