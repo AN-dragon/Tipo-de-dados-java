2202ee0d37d82714800064b4ca9cc4f3324c01d6
AN-dragon <spectdragoniv@gmail.com>
2023-03-07 14:34:22 +0000

    Roses

---
+++ atv05_Pizza.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class atv05_Pizza {
+    
+    public static void main(String[] args) {
+
+        //Exercício 4
+
+        Scanner input = new Scanner(System.in);
+        String nome;
+        int idade;
+
+        //Primeira pessoa
+        System.out.println("pessoa1, diga seu nomeUwU: ");
+        nome = input.next();
+
+        System.out.println("agora sua idade: ");
+        idade = input.nextInt();
+
+        //Segunda pessoa
+        System.out.println("pessoa2, diga seu nomeUwU: ");
+        nome = input.next();
+
+        System.out.println("agora sua idade: ");
+        idade = input.nextInt();
+
+
+    }
+}
